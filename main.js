14290677f301da6b23b6c7765f39c3a963bf3b16
Mok2109 <76574359+Mok2109@users.noreply.github.com>
2021-07-08 18:49:30 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot()
 }
 
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/-1qukqaae/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/pEQYY41Nv/',modelLoaded);
 
 function modelLoaded(){
     console.log('Model Loaded!');
@@ -26,7 +26,7 @@ function modelLoaded(){
 function check()
 {
     img = document.getElementById('captured_image');
-    classifier.classify(img, gotResult)
+    classifier.classify(img, gotResult);
 }
 
 function gotResult(error, results) {
@@ -38,4 +38,37 @@ function gotResult(error, results) {
         document.getElementById("result_object_name").innerHTML = results[0].label;
         document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
     }
+}
+
+function place_to_recycle()
+{
+    if(results[0].label == "Plastic 1")
+    {
+        document.getElementById("result_object_place").innerHTML = "Sanjose Recyling Center";
+    }
+    
+    if(results[0].label == "Plastic 2"){
+        document.getElementById("result_object_place").innerHTML = "Sanjose Recyling Center";
+    }
+    if(results[0].label == "Plastic 3"){
+        document.getElementById("result_object_place").innerHTML = "Cannot Be recylced but can be donated to places such as Goodwill and Salvation Army";
+    }
+    if(results[0].label == "Plastic 4"){
+        document.getElementById("result_object_place").innerHTML = "Sanjose Recyling Center";
+    }
+
+    if(results[0].label == "Plastic 5"){
+        document.getElementById("result_object_place").innerHTML = "Sanjose Recyling Center";
+    }
+
+    if(results[0].label == "Plastic 6"){
+    document.getElementById("result_object_place").innerHTML = "Sanjose Recyling Center";
+    }
+
+    if(results[0].label == "Plastic 7"){
+        document.getElementById("result_object_place").innerHTML = "Sanjose Recyling Center";
+    }
+
+
+
 }
